02a2f927149fed4da07c2dfa8fd8c52a154a9a10
unknown <Hamza Imam Amjad>
2020-10-24 07:40:55 +0500

    deployment by hamza

--- api_products.py
+++ api_products.py
@@ -33,10 +33,11 @@ def calculation(product, working_hours):
         avg_energy_cons = required[0][1] * 360
         my_array = {
             'product' : required[0][0],
-            'avg_energy_cons' :  avg_energy_cons
+            'avg_energy_cons' :  avg_energy_cons, 
         }
         co_emissions = 600 * avg_energy_cons * int(working_hours)
-        my_array['co_emissions'] = co_emissions
+        my_array['co_emissions'] = co_emissions / 100
+        my_array['savings_euros'] = my_array['co_emissions'] * 0.0132
         sorted_dict = recommendationSystem(int(working_hours))
         my_array['top3'] = sorted_dict
         return my_array
@@ -47,7 +48,8 @@ def recommendationSystem(working_hours):
     data = pandas.read_excel('products_database.xlsx', sheet_name='Products')
     df = pandas.DataFrame(data)
     df['Avg cons'] = df['Per day cons'] * 360
-    df['co2_emissions'] = working_hours * 600 * df['Avg cons']
+    df['co2_emissions'] = (working_hours * 600 * df['Avg cons'])/100
+    df['savings_euros'] = 0.0132 * df['co2_emissions'] 
     df['rank'] = df.index + 1
     df = df.sort_values(by='co2_emissions', ascending=False)
     df = df.iloc[:3]
